a8694310fee657248894790fcee5e760b94add72
Thomas Tisch <t.tisch@live.com>
2025-01-03 00:15:15 +0100

    Add reschedule button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 
-import { IconAdjustments } from '@tabler/icons-react';
+import { useState } from 'react';
+import { IconAdjustments, IconRouteScan } from '@tabler/icons-react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { ActionIcon, AppShell, Container, MantineProvider, Text } from '@mantine/core';
 import { BusinessHoursProvider } from './contexts/BusinessHoursContext';
@@ -9,6 +10,16 @@ import { theme } from './theme';
 
 export default function App() {
   const navigate = useNavigate();
+  const [rescheduling, setRescheduling] = useState(false);
+
+  const handleClickReschedule = () => {
+    console.log('Reschedule');
+    setRescheduling(true);
+    setTimeout(() => {
+      setRescheduling(false);
+    }, 2000);
+  };
+
   return (
     <LanguageProvider>
       <BusinessHoursProvider>
@@ -26,6 +37,9 @@ export default function App() {
                   >
                     Agenda
                   </Text>
+                  <ActionIcon variant="filled" size="sm" onClick={handleClickReschedule}>
+                    <IconRouteScan style={{ width: '70%', height: '70%' }} stroke={1.5} />
+                  </ActionIcon>
                   <ActionIcon
                     variant="filled"
                     size="sm"
@@ -40,9 +54,7 @@ export default function App() {
               </Container>
             </AppShell.Header>
             <AppShell.Main>
-              <Container>
-                <Outlet />
-              </Container>
+              <Container>{!rescheduling ? <Outlet /> : <Text>Rescheduling ...</Text>}</Container>
             </AppShell.Main>
           </AppShell>
         </MantineProvider>
