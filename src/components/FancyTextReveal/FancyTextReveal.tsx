abc99d5cae617160199c8fdebac6b15458a2ba44
Thomas Tisch <t.tisch@live.com>
2025-01-11 23:18:52 +0100

    Adjust styling

--- src/components/FancyTextReveal/FancyTextReveal.tsx
+++ src/components/FancyTextReveal/FancyTextReveal.tsx
@@ -2,7 +2,7 @@ import VerticalCutReveal from './../fancy/vertical-cut-reveal';
 
 export default function Preview() {
   return (
-    <div className="w-full h-full xs:text-2xl text-2xl sm:text-4xl md:text-7xl lg:text-7xl xl:text-7xl flex flex-col items-start justify-center font-overusedGrotesk bg-background p-10 md:p-16 lg:p-24 text-[#555] tracking-wide uppercase">
+    <div className="w-full xs:text-2xl text-2xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-5xl flex flex-col items-start justify-center font-overusedGrotesk bg-background text-[#555] tracking-wide uppercase">
       <VerticalCutReveal
         splitBy="characters"
         staggerDuration={0.025}
@@ -15,6 +15,7 @@ export default function Preview() {
       >
         TURN YOUR DEADLINES ⏰ ...
       </VerticalCutReveal>
+      <br />
       <VerticalCutReveal
         splitBy="characters"
         staggerDuration={0.025}
@@ -27,8 +28,9 @@ export default function Preview() {
           delay: 0.5,
         }}
       >
-        ... INTO YOUR HEADLINES 📰
+        ... INTO HEADLINES 📰
       </VerticalCutReveal>
+      <br />
       <VerticalCutReveal
         splitBy="characters"
         staggerDuration={0.025}
@@ -37,7 +39,7 @@ export default function Preview() {
           type: 'spring',
           stiffness: 200,
           damping: 21,
-          delay: 1.1,
+          delay: 1.5,
         }}
       >
         {`LET'S GO 😊`}
--- src/pages/Test.page.tsx
+++ src/pages/Test.page.tsx
@@ -2,8 +2,8 @@ import FancyTextReveal from '@/components/FancyTextReveal/FancyTextReveal';
 
 export function TestPage() {
   return (
-    <>
+    <div className="w-screen flex items-center justify-center pl-32 pt-48">
       <FancyTextReveal />
-    </>
+    </div>
   );
 }
